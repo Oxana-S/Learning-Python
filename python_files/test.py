51060548da3ae8db094ba4afa7283fc9be22fb71
Sergio Yarkov <channel.my.honey@gmail.com>
2022-03-24 15:51:34 +0300

    Первый коммит

---
+++ python_files/test.py
@@ -0,0 +1,15 @@
+ru_word = "Привет"  #string
+en_word = "Hello"  #string
+
+list1 = [1, 2, 3]  #int
+tuple1 = (4, 5, 6)  #int
+set1 = {7.91, 8.83, 9.77}  #float
+dic1 = {'a': 10, 'b': 20, 'c': 30}  #dict
+
+a = 1  #int
+b = 2  #float
+
+print(ru_word)
+print(ru_word)
+print("Работаю с Git")
+print("Работаю с Git. Тестирую основные команды")
